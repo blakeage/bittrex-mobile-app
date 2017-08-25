1b51542ebf71f07b3657e47449762042007f0759
Blake Miller <blakeleemiller@gmail.com>
2017-08-25 19:38:34 -0400

    Convert tabs to spaces

--- app/actions/market_summary.js
+++ app/actions/market_summary.js
@@ -13,38 +13,38 @@ export function requestMarketSummary() {
 }
 
 function marketSummaryError(error) {
-	console.warn(error);
-	return {
+  console.warn(error);
+  return {
     type: MARKET_SUMMARY_ERROR,
-		error
+    error
   };
 }
 
 export function fetchMarketSummary() {
-	return (dispatch) => {
-		if(ApiHelper.stubbing()) {
-			dispatch(receiveMarketSummary(market_summary_data));	
-			return;
-		}
+  return (dispatch) => {
+    if(ApiHelper.stubbing()) {
+      dispatch(receiveMarketSummary(market_summary_data));  
+      return;
+    }
 
-		var uri = ApiHelper.getApiUri('/public/getmarketsummaries');
-		fetch(uri, {
-			method: 'GET',
-			headers: { 'apisign': ApiHelper.getSignature(uri) }
-		})
-		.then(response => response.json())
-		.then(json => dispatch(receiveMarketSummary(json)))
-		.catch(error => dispatch(marketSummaryError(error)));
-	};
+    var uri = ApiHelper.getApiUri('/public/getmarketsummaries');
+    fetch(uri, {
+      method: 'GET',
+      headers: { 'apisign': ApiHelper.getSignature(uri) }
+    })
+    .then(response => response.json())
+    .then(json => dispatch(receiveMarketSummary(json)))
+    .catch(error => dispatch(marketSummaryError(error)));
+  };
 }
 
 export function receiveMarketSummary(json) {
-	let ms = new MarketSummary();
-	let results = json.result;
+  let ms = new MarketSummary();
+  let results = json.result;
 
-	if(results.length > 0) {
-		ms.setCoinSummaries(results);
-	}
+  if(results.length > 0) {
+    ms.setCoinSummaries(results);
+  }
   return {
     type: RECEIVE_MARKET_SUMMARY,
     market_summary: ms 
