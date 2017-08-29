4809c8b390392e38e302627a2a91742d8bfe68cf
Blake Miller <blakeleemiller@gmail.com>
2017-08-28 20:57:41 -0400

    Change url

--- generate_sign.php
+++ generate_sign.php
@@ -1,9 +1,9 @@
 <?php
-	$apikey='';
-	$apisecret='';
-	$nonce=time();
-	$uri='https://bittrex.com/api/v1.1/market/getopenorders?apikey='.$apikey.'&nonce='.$nonce;
-	$sign=hash_hmac('sha512',$uri,$apisecret);
-	echo "\n" . $sign . "\n";
-	echo $nonce . "\n";
+  $apikey='';
+  $apisecret='';
+  $nonce=time();
+  $uri='https://bittrex.com/api/v1.1/account/getbalance?apikey='.$apikey.'&currency=ADX&nonce='.$nonce;
+  $sign=hash_hmac('sha512',$uri,$apisecret);
+  echo "\n" . $sign . "\n";
+  echo $nonce . "\n";
 ?>
