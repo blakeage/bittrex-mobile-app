de127b063f6bf5731b3733f445622295fcf6a30f
Blake Miller <blakeleemiller@gmail.com>
2017-08-28 20:56:51 -0400

    Add console warning for failed api requests

--- app/actions/open_orders.js
+++ app/actions/open_orders.js
@@ -14,6 +14,7 @@ export function requestOpenOrders() {
 }
 
 function openOrdersError(error) {
+  console.warn(error);
   return {
     type: OPEN_ORDERS_ERROR,
     error
--- app/actions/order_history.js
+++ app/actions/order_history.js
@@ -14,6 +14,7 @@ export function requestOrderHistory() {
 }
 
 function orderHistoryError(error) {
+  console.warn(error);
   return {
     type: ORDER_HISTORY_ERROR,
     error
